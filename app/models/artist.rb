48d5af44b7058feb03d488da7cbb7f8d31c01dc6
nicolewang2000 <nicole.wang@yale.edu>
2020-05-31 14:31:46 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,40 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select{|painting|painting.artist == self}
+  end 
+
+  def galleries
+    paintings.map{|painting| painting.gallery}
+  end
+
+  def cities
+    galleries.map{|gallery| gallery.city}
+  end 
+
+  def self.total_experience
+    self.all.reduce(0) {|total, artist| total + artist.years_experience}
+  end 
+
+  def self.most_prolific
+    self.all.max_by {|artist| artist.paintings.count / artist.years_experience}
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
   end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,23 @@
 class Painting
 
   attr_reader :title, :price
+  attr_accessor :artist, :gallery
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, artist, gallery)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    self.all.reduce(0) {|total, painting| total + painting.price}
+  end 
 end
